e172b0513488eeb3c4e8f043a743c7c575cdeb12
BartoszBanaszek <129880@student.uksw.edu.pl>
2026-03-07 17:50:01 +0100

    Update Circle.cpp

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,22 +1,4 @@
 
 #include "Circle.h"
 
-template <class T>
-void Circle<T>::compute() {
-    T radius=this->param->get(PARAM_RADIUS);
-
-const double PI=3.14159265358979323846;
-this->area=static_cast<T>(PI*radius*radius);
-this->perimeter=static_cast<T>(2*PI*radius);
-}
-
-template <class T> void Circle<T>::print() {  
-    T radius = this->param->get(PARAM_RADIUS); 
-    std::cout << "=== FIGURA: KOLO ===" << std::endl; 
-    std::cout << "Promien: " << radius << std::endl; 
-    std::cout << "Pole powierzchni: " << this->area << std::endl; 
-    std::cout << "Obwod: " << this->perimeter << std::endl; 
-    std::cout << "====================" << std::endl; 
-} 
-
 
